312fe8194f9dbf74d595aa499800fa65abb210c4
adenilsonalves_compasso <adenilsonalves_compasso@tupperware.com>
2024-10-29 10:43:34 -0300

    fix(routes)

--- launch.py
+++ launch.py
@@ -3,5 +3,11 @@ helloworld = Flask(__name__)
 @helloworld.route("/")
 def run():
     return "{\"message\":\"Hello World Python v2\"}"
+
+@helloworld.route("/flask")
+def web():
+    return "{\"message\":\"EndpointWeb\"}"
+
+
 if __name__ == "__main__":
     helloworld.run(host="0.0.0.0", port=int("8080"), debug=True)
